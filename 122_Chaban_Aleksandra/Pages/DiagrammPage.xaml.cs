da619fb5890028746cf103a8a6663e0ba94dd5e1
Aleksandra <blueberry04p@gmail.com>
2025-10-28 09:49:39 +0300

    Готово 2

--- 122_Chaban_Aleksandra/Pages/DiagrammPage.xaml.cs
+++ 122_Chaban_Aleksandra/Pages/DiagrammPage.xaml.cs
@@ -113,55 +113,79 @@ namespace _122_Chaban_Aleksandra
 
         private void BtnExportToWord_Click(object sender, RoutedEventArgs e)
         {
-            try
+            var allUsers = _context.Users.ToList();
+            var allCategories = _context.Category.ToList();
+
+            var application = new Word.Application();
+            Word.Document document = application.Documents.Add();
+
+            foreach (var user in allUsers)
             {
-                var allUsers = _context.Users.ToList().OrderBy(p => p.FIO).ToList();
+                Word.Paragraph userParagraph = document.Paragraphs.Add();
+                Word.Range userRange = userParagraph.Range;
+                userRange.Text = user.FIO;
+                userParagraph.set_Style("Заголовок");
+                userRange.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                userRange.InsertParagraphAfter();
+                document.Paragraphs.Add(); //Пустая строка 
 
-                var application = new Excel.Application();
-                Excel.Workbook workbook = application.Workbooks.Add();
+                Word.Paragraph tableParagraph = document.Paragraphs.Add();
+                Word.Range tableRange = tableParagraph.Range;
+                Word.Table paymentsTable = document.Tables.Add(tableRange, allCategories.Count() + 1, 2);
+                paymentsTable.Borders.InsideLineStyle = paymentsTable.Borders.OutsideLineStyle =
+                 Word.WdLineStyle.wdLineStyleSingle;
+                paymentsTable.Range.Cells.VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
 
-                // Создаем один лист для всех пользователей
-                Excel.Worksheet worksheet = workbook.ActiveSheet;
-                worksheet.Name = "Все платежи";
+                Word.Range cellRange;
+                cellRange = paymentsTable.Cell(1, 1).Range;
+                cellRange.Text = "Категория";
+                cellRange = paymentsTable.Cell(1, 2).Range;
+                cellRange.Text = "Сумма расходов";
+                paymentsTable.Rows[1].Range.Font.Name = "Times New Roman"; paymentsTable.Rows[1].Range.Font.Size = 14;
+                paymentsTable.Rows[1].Range.Bold = 1;
+                paymentsTable.Rows[1].Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphCenter;
 
-                int rowIndex = 1;
-
-                // Заголовки
-                worksheet.Cells[rowIndex, 1] = "Пользователь";
-                worksheet.Cells[rowIndex, 2] = "Дата платежа";
-                worksheet.Cells[rowIndex, 3] = "Категория";
-                worksheet.Cells[rowIndex, 4] = "Название";
-                worksheet.Cells[rowIndex, 5] = "Стоимость";
-                worksheet.Cells[rowIndex, 6] = "Количество";
-                worksheet.Cells[rowIndex, 7] = "Сумма";
-
-                rowIndex++;
-
-                // Данные
-                foreach (var user in allUsers)
+                for (int i = 0; i < allCategories.Count(); i++)
                 {
-                    foreach (var payment in user.Paymant.OrderBy(p => p.Date))
-                    {
-                        worksheet.Cells[rowIndex, 1] = user.FIO;
-                        worksheet.Cells[rowIndex, 2] = payment.Date.ToString("dd.MM.yyyy HH:mm");
-                        worksheet.Cells[rowIndex, 3] = payment.Category?.Name;
-                        worksheet.Cells[rowIndex, 4] = payment.Name;
-                        worksheet.Cells[rowIndex, 5] = payment.Price;
-                        worksheet.Cells[rowIndex, 6] = payment.Num;
-                        worksheet.Cells[rowIndex, 7].Formula = $"=E{rowIndex}*F{rowIndex}";
+                    var currentCategory = allCategories[i];
+                    cellRange = paymentsTable.Cell(i + 2, 1).Range;
+                    cellRange.Text = currentCategory.Name;
+                    cellRange.Font.Name = "Times New Roman";
+                    cellRange.Font.Size = 12;
+                    cellRange = paymentsTable.Cell(i + 2, 2).Range;
+                    cellRange.Text = user.Paymant.ToList().
+                   Where(u => u.Category == currentCategory).Sum(u => u.Num * u.Price).ToString("N2") + " руб.";
+                    cellRange.Font.Name = "Times New Roman";
+                    cellRange.Font.Size = 12;
+                } //завершение цикла по строкам таблицы 
+                document.Paragraphs.Add(); //пустая строка 
 
-                        rowIndex++;
-                    }
+                Paymant maxPayment = user.Paymant.OrderByDescending(u => u.Price * u.Num).FirstOrDefault();
+                if (maxPayment != null)
+                {
+                    Word.Paragraph maxPaymentParagraph = document.Paragraphs.Add(); Word.Range maxPaymentRange = maxPaymentParagraph.Range; maxPaymentRange.Text = $"Самый дорогостоящий платеж -  { maxPayment.Name} за { (maxPayment.Price * maxPayment.Num).ToString("N2")} " + $"руб.от { maxPayment.Date.ToString("dd.MM.yyyy")}  "; 
+                    maxPaymentParagraph.set_Style("Подзаголовок");
+                    maxPaymentRange.Font.Color = Word.WdColor.wdColorDarkRed;
+                    maxPaymentRange.InsertParagraphAfter();
                 }
+                document.Paragraphs.Add(); //пустая строка 
 
-                worksheet.Columns.AutoFit();
-                application.Visible = true;
-                MessageBox.Show("Данные успешно экспортированы в Excel!");
+                Paymant minPayment = user.Paymant.OrderBy(u => u.Price * u.Num).FirstOrDefault();
+                if (maxPayment != null)
+                {
+                    Word.Paragraph minPaymentParagraph = document.Paragraphs.Add(); Word.Range minPaymentRange = minPaymentParagraph.Range; minPaymentRange.Text = $"Самый дешевый платеж - {minPayment.Name}   за { (minPayment.Price * minPayment.Num).ToString("N2")} " + $"руб.от  { minPayment.Date.ToString("dd.MM.yyyy")} "; 
+                minPaymentParagraph.set_Style("Подзаголовок");
+                    minPaymentRange.Font.Color = Word.WdColor.wdColorDarkGreen;
+                    minPaymentRange.InsertParagraphAfter();
+                }
+                if (user != allUsers.LastOrDefault()) document.Words.Last.InsertBreak(Word.WdBreakType.wdPageBreak);
+
+                
+
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            application.Visible = true;
+
+
         }
-    }
+        }
 }
